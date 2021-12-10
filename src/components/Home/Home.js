a8e9df7d6f32da8a3a90c5469512947b7bba2930
Niraj <niraj2323@gmail.com>
2021-12-10 13:28:14 -0500

    Completed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,6 +12,79 @@ class Home extends React.Component{
       <div className="home">
         <video id="home-video" autoPlay loop muted src={Video} type='video/mp4'>
         </video>
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+
+        <div class="tags">
+    <div class="tag-container">
+        <div class="tag">
+
+            <div class="tag-side tag-1-side">
+                <div class="tag-1-top"></div>
+                <div class="tag-text tag-1-text">
+                    Merry<br />Christmas
+                    <div class="rule-shape rule-red">⭐</div>
+                </div>
+            </div>
+
+            <div class="tag-side tag-1-side is-back">
+                <div class="tag-1-top"></div>
+                <div class="tag-text tag-1-text">
+                    And a happy New Year
+                    <div class="rule-shape">⭐</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="tag-container">
+        <div class="tag">
+
+            <div class="tag-side tag-2-side">
+                <div class="tag-text tag-2-text">
+                    Happy<br />Holidays
+                    <div class="rule-diagonal"></div>
+                </div>
+            </div>
+
+            <div class="tag-side tag-2-side is-back">
+                <div class="tag-text tag-2-text">
+                    May your every wish come true
+                    <div class="rule-diagonal"></div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="tag-container">
+        <div class="tag">
+
+            <div class="tag-side tag-3-side">
+                <div class="tag-text tag-3-text">
+                    Feliz<br />Navidad
+                    <div class="rule-shape">&#10052;</div>
+                </div>
+            </div>
+
+            <div class="tag-side tag-3-side is-back">
+                <div class="tag-text tag-3-text">
+                    Prospero Año Nuevo
+                    <div class="rule-shape">&#10052;</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
       {this.props.login ? (
         <div className="container">
         <TitleContainer>
